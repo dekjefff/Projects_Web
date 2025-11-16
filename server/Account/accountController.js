e58b094f994be52f4bab66eec7fb63e41e89d164
dekjefff <jaffephillips.gmail.com>
2025-11-16 22:38:00 +0700

    new version of server that can verify account

--- server/Account/accountController.js
+++ server/Account/accountController.js
@@ -8,24 +8,34 @@ dotenv.config();
 
 exports.signIn = (req,res) => {
 
-    const {user_ID , user_name, passwd} = req.body;
+    const {user_name, passwd} = req.body;
+    const user_ID = Date.now().toString();  
     const hashPassword = bcrypt.hashSync(passwd, 8);
 
-    if(!user_ID || !user_name|| !passwd){
-        return res.status(404).send({error: true});
+    if(!user_name|| !passwd){
+        return res.status(400).send({error: true, message: error.message});
     };
 
+    const sqlCheck = "SELECT * FROM UserAccount WHERE user_name = ?";
+    DBcon.query(sqlCheck, [user_name], (error, results) => {
+    if (error) return res.status(500).send({ error: true, message: error.message });
+
+    if (results.length > 0) {
+      return res.status(400).send({message: "user name is exists" });
+    }
+
     DBcon.query("INSERT INTO UserAccount set ?", {user_ID, user_name, passwd: hashPassword} , function(error,results){
-        if(error) throw error;
-        return res.send({
-            error: false,
-            data: results,
-            message: "New account has been created succesfully"
+    if (error) return res.status(500).send({ error: true, message: error.message });
+    return res.send({
+        error: false,
+        data: results,
+        user_ID,
+        message: "New account has been created succesfully"
         });
 
     });
-
-};
+});
+}
 
 exports.logIn = (req,res) => {
 
@@ -33,7 +43,8 @@ exports.logIn = (req,res) => {
 
     DBcon.query("SELECT * FROM UserAccount WHERE user_ID = ?  OR user_name = ?", [user_ID , user_name] , (error,results) => {
 
-        if(error) throw error;
+
+        if (error) return res.status(500).send({ error: true, message: error.message });
         if(results.length === 0) return res.status(404).send({message: "User Acount Not Found"});
 
         const user = results[0];
@@ -41,7 +52,7 @@ exports.logIn = (req,res) => {
 
         if(!passwdIsvalid) return res.status(401).send({message: "Invalid password"});
 
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+        const token = jwt.sign({user_name: user.user_name}, process.env.JWT_SECRET, {expiresIn: "1d"});
 
         res.send({message: "Login Sucessful", token})
         });
@@ -50,13 +61,14 @@ exports.logIn = (req,res) => {
 
 exports.verifyToken = (req,res,next) => {
 
-    const token = req.headers["authorization"];
-    if(!token) return res.status(403).send({message: "No token provided"});
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) return res.status(403).send({message: "No token provided"});
 
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SECRET, (error,decoded) => {
 
         if(error) return res.status(401).send({message: "Unauthorization"});
-        req.user_ID = decoded.user_ID;
+        req.user_name = decoded.user_name;
         next();
         
     });
--- server/Account/accountRouter.js
+++ server/Account/accountRouter.js
@@ -5,6 +5,14 @@ const accountController = require('./accountController');
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
+
+// URL for register:  http://localhost:3030/signin
+// Body
+// {
+//   "user_ID": "0006787095",
+//   "user_name": "dekJeff",
+//   "passwd": "123456"
+// }
 router.post("/signin", accountController.signIn);
 router.post("/login", accountController.logIn);
 
