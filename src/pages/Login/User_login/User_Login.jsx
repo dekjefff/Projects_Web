a677cb5b435a4b612a225664551d62807a7421d0
dekjefff <jaffephillips.gmail.com>
2025-11-16 22:45:16 +0700

    new version of user login can call qpi from server

--- src/pages/Login/User_login/User_Login.jsx
+++ src/pages/Login/User_login/User_Login.jsx
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './User_Login.css';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 const User_Login = () => {
+
+    const [form, setForm] = useState({
+      user_name: '',
+      passwd: '',
+    });
+
+    const [msg, setMsg] = useState('');
+    const navigate = useNavigate();
+    
+      function handleChange(e) {
+        const { name, value } = e.target;
+        setForm(prev => ({ ...prev, [name]: value }));
+      }
+    
+      async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+          const res = await fetch('http://localhost:3030/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(form)
+          });
+
+          const data = await res.json();
+          
+          if (!res.ok){
+            setMsg(data.message);
+            alert(data.message);
+            return;
+          } 
+
+        
+          
+          navigate('/');
+          setMsg(data.message || 'Registered successfully');
+        } catch (error) {
+          setMsg('Error: ' + error.message);
+        }
+      }
+
   return (
     <div className="login-page">
       <header className="header">
@@ -11,7 +53,7 @@ const User_Login = () => {
           <div className="logo-section">
             <img src="src/assets/Logo.png" alt="Logo" />
           </div>
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleSubmit}>
             <div className="input-group">
               <img src="src/assets/user-icon.png" alt="User Icon" className="input-icon-img" />
               <input
@@ -19,6 +61,9 @@ const User_Login = () => {
                 placeholder="Username"
                 required
                 className="input-field"
+                name="user_name"
+                value={form.user_name}
+                onChange={handleChange}
               />
               <span className="required-star">*</span>
             </div>
@@ -29,6 +74,9 @@ const User_Login = () => {
                 placeholder="Password"
                 required
                 className="input-field"
+                name="passwd"
+                value={form.passwd}
+                onChange={handleChange}
               />
               <span className="required-star">*</span>
             </div>
