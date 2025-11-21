fd3c88b4ec67921fcb6b4cf14175928e3270ab82
Pakinnar <pakin.nar@student.mahidol.edu>
2025-11-21 23:44:14 +0700

    Update teamprofile.jsx

--- src/pages/Team/teamprofile.jsx
+++ src/pages/Team/teamprofile.jsx
@@ -9,43 +9,43 @@ const TeamDev = () => {
       name: 'วีรากร ตรีศิริภพ',
       studentId: '6787076',
       instagram: 'Fong_vrt',
-      image: '/src/assets/Fong.jpg', 
+      image: '/src/assets/Fong.jpg',
     },
     {
       id: 2,
-      name: 'สุขกฤษฎิ์ ชัยวาลย์',
+      name: 'สุกฤษฎิ์ ชัยวาลย์',
       studentId: '6787083',
       instagram: 'firsst_ss',
-      image: '/src/assets/First.jpg', 
+      image: '/src/assets/First.jpg',
     },
     {
       id: 3,
       name: 'เจฟฟี่ เอมิก้า ฟิลิปส์',
       studentId: '6787094',
       instagram: 'dekjeff',
-      image: '/src/assets/jeff.jpeg', 
+      image: '/src/assets/jeff.jpeg',
     },
     {
       id: 4,
       name: 'ภาคิน นาคเจริญ',
       studentId: '6787113',
       instagram: 'pakin_llll',
-      image: '/src/assets/ball.jpg', 
+      image: '/src/assets/ball.jpg',
     },
   ];
 
   return (
     <div className="teamdev-container">
-        <navlogo-component />
-
+      <navlogo-component />
+      <div class="line"></div>
       <div className="teamdev-content">
         <h1 className="teamdev-title">Team Developer</h1>
 
-        <div className="team-members-grid">
+        <div className="team-container">
           {teamMembers.map((member, index) => (
-            <div 
-              key={member.id} 
-              className={`member-card ${index === 0 ? 'selected-member' : ''}`}
+            <div
+              key={member.id}
+              className={`team-card ${index === 0 ? 'selected-member' : ''}`}
             >
               <div className="member-image-wrapper">
                 <img src={member.image} alt={member.name} className="member-image" />
@@ -53,7 +53,10 @@ const TeamDev = () => {
               <h3 className="member-name">{member.name}</h3>
               <p className="member-id">{member.studentId}</p>
               <p className="member-instagram">
-                <span className="instagram-icon">📷</span> {member.instagram}
+                <span className="instagram-icon">
+                  <img src="src\assets\Instragram-logo.png" width='20px' height='20px' alt="ig" />
+                </span>
+                {member.instagram}
               </p>
             </div>
           ))}
@@ -61,7 +64,7 @@ const TeamDev = () => {
       </div>
 
       {/* Footer (เหมือนกับหน้า ProfileCard) */}
-        <footer-main-component />
+          <footer-login-component />
     </div>
   );
 };
